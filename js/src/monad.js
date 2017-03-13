b885b30ff8e2eb44c934213f67a136cfb033b4d3
Matthew Franglen <matthew@franglen.org>
2017-03-13 21:36:01 +0000

    Complete applicatives
    Given up on the idea of any applicative applied to any other - can't
    reconcile the types.

--- js/src/monad.js
+++ js/src/monad.js
@@ -44,6 +44,9 @@ class MaybeFunctor extends Maybe {
 // An applicative of type T holds values of type T and provides apply which takes another applicative holding a function of type (T -> U)
 // In this I will assume that the two applicatives are of the same type (i.e. ArrayApplicative applied to an ArrayApplicative, etc)
 // If I can generalize this then that would be good.
+// I don't think it's generalizable.
+// What would be the return type of applying an array of functions to a maybe? How does that compare to applying a maybe function to an array?
+// Since those two cannot be consistent in return type with each other, avoid layering AND return all values I think it is impossible.
 
 class ArrayApplicative extends ArrayFunctor {
 
@@ -54,10 +57,20 @@ class ArrayApplicative extends ArrayFunctor {
 
 }
 
+// aa = new ArrayApplicative(1, 2, 3)
+// af = new ArrayApplicative(a => a + 1, a => a * 2)
+// aa.apply(af)
+// ➜ [ 2, 3, 4, 2, 4, 6 ]
+
 class MaybeApplicative extends MaybeFunctor {
 
   apply (f) {
-    return f.fmap(fun => this.fmap(fun));
+    return f.fmap(fun => this.fmap(fun)).value;
   }
 
 }
+
+// ma = new MaybeApplicative(1)
+// mf = new MaybeApplicative(a => a * 3)
+// ma.apply(mf)
+// ➜ { 3 }
