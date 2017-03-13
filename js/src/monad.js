981137020ac1aacd0d3dd659505a0a2a71068e4d
Matthew Franglen <matthew@franglen.org>
2017-03-13 21:46:23 +0000

    Implement monads

--- js/src/monad.js
+++ js/src/monad.js
@@ -14,6 +14,10 @@ class ArrayFunctor extends Array {
 
 }
 
+// af = new ArrayFunctor(1, 2, 3)
+// af.apply(a => a + 1)
+// ➜ [ 2, 3, 4 ]
+
 // A maybe is a functor that applies the function to the held value if it is present
 // A maybe is a holder for zero or one value of type T.
 
@@ -41,6 +45,10 @@ class MaybeFunctor extends Maybe {
 
 }
 
+// mf = new MaybeFunctor(1)
+// mf.apply(a => a * 3)
+// ➜ { 3 }
+
 // An applicative of type T holds values of type T and provides apply which takes another applicative holding a function of type (T -> U)
 // In this I will assume that the two applicatives are of the same type (i.e. ArrayApplicative applied to an ArrayApplicative, etc)
 // If I can generalize this then that would be good.
@@ -65,7 +73,8 @@ class ArrayApplicative extends ArrayFunctor {
 class MaybeApplicative extends MaybeFunctor {
 
   apply (f) {
-    return f.fmap(fun => this.fmap(fun)).value;
+    return f.fmap(fun => this.fmap(fun))
+            .value;
   }
 
 }
@@ -73,4 +82,34 @@ class MaybeApplicative extends MaybeFunctor {
 // ma = new MaybeApplicative(1)
 // mf = new MaybeApplicative(a => a * 3)
 // ma.apply(mf)
+// ➜ { 3 }
+
+// A monad of type T holds values of type T and provides bind which takes a function of type (T -> monad U)
+// Javascript is unlikely to care if the monad in the return is the same type, however most monads will naturally have that restriction.
+// This is because the value returned by the bind method may not be the one returned by the function.
+
+class ArrayMonad extends ArrayApplicative {
+
+  bind (f) {
+    return this.fmap(f)
+               .reduce((a, v) => a.concat(v));
+  }
+
+}
+
+// am = new ArrayMonad(1, 2, 3)
+// aa.apply(a => [a, a * 2])
+// ➜ [ 1, 2, 2, 4, 3, 6 ]
+
+class MaybeMonad extends MaybeApplicative {
+
+  bind (f) {
+    return this.fmap(f)
+               .value;
+  }
+
+}
+
+// mm = new MaybeMonad(1)
+// mm.bind(a => new MaybeMonad(a * 3))
 // ➜ { 3 }
