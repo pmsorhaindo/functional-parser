774172820acc7ec1fb416ef1767a8996fc1f138a
Matthew Franglen <matthew@franglen.org>
2017-03-15 21:00:17 +0000

    I hate javascript
    The instance of ArrayApplicative created by this lacks the pure and
    apply methods. WTF?

--- js/src/__tests__/monad.spec.js
+++ js/src/__tests__/monad.spec.js
@@ -1,7 +1,13 @@
 import { ArrayFunctor, ArrayApplicative, ArrayMonad, Maybe, MaybeFunctor, MaybeApplicative, MaybeMonad } from '../monad';
 
 describe('applicative laws', () => {
-  it('should adhere to the identity law', () => {
+  const pure = value => new ArrayApplicative(value);
+  const id = value => value;
 
+  // pure id <*> v = v
+  it('should adhere to the identity law', () => {
+    const v = "value";
+
+    expect(pure(v).apply(pure(id))).toEqual(pure(v));
   });
 });
